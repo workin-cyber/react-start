c5aed33ab7506e402f4313f193d09bc215d3f142
unknown <contact@workin.co.il>
2021-07-27 15:40:14 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './style.css'
-import Clock from './Clock'
 
 ReactDOM.render(
   <React.StrictMode>
     <h1>Hello world!</h1>
-    <Clock />
   </React.StrictMode>,
   document.getElementById('root')
 )
